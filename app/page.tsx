628eef6939249700eae9db98e44422f889f51821
Rayhan Atmadja <workspace.rayhanatmadja@gmail.com>
2026-06-29 16:55:18 +0700

    fix(res): reduce padding

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { Navigation } from "@/components/ui/Navigation";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-6 md:px-16 sm:items-start">
         <TimeResolutionHeader />
         <Navigation />
         <IntroSection />
